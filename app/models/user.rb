0b6866aa862d1ac6542dea4a256bc2c4c36e1871
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-10-26 14:11:38 +0900

    messagesテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name,presence: true  #空の場合はDBに保存しない
-  
   has_many :room_users
   has_many :rooms, through: :room_users
+  has_many :messages
+  
+  validates :name,presence: true  #空の場合はDBに保存しない
 end
